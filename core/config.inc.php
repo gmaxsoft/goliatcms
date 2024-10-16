6c67c849da74fd3c856c1075bab63a735059f92e
Grzegorz Miśków <133238725+gmaxsoft@users.noreply.github.com>
2024-10-16 19:29:51 +0200

    Update config.inc.php

--- core/config.inc.php
+++ core/config.inc.php
@@ -9,13 +9,13 @@ define("TIMEZONE", "Europe/Warsaw");
 define("BASE", 'https://' . $_SERVER['HTTP_HOST']);
 
 /* CMS DATABASE PARAMS */
-define('DB_HOST', '23113.m.tld.pl');
-define('DB_NAME', 'baza23113_crm');
-define('DB_USERNAME', 'admin23113_crm');
-define('DB_PASSWORD', 'w0O}b8R{e5');
+define('DB_HOST', '');
+define('DB_NAME', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
 define('DB_CHARSET', 'utf8');
 define('DB_SOCKET', '');
-define('DB_PORT', '3306');
+define('DB_PORT', '');
 define('DB_PREFIX', 'cms_');
 
 /* CMS SESSION PARAMS */
